9768354a8ee1aa38a7a20c7b5f1ec339275c7045
Llala <rene.raboshakga@gmail.com>
2023-10-31 19:00:56 +0200

    Task 8 converting lowercase to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    upp_str = ""
+    for char in str:
+        if 'a'<= char <= 'z':
+            upp_char = chr(ord(char) - 32)
+            upp_str += upp_char
+        else:
+            upp_str += char
+    print("{}".format(upp_str))
