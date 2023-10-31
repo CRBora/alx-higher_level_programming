845b626a61588b60cac36675420f72e2a45d7ad7
Llala <rene.raboshakga@gmail.com>
2023-10-31 19:49:56 +0200

    Task 12 FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/pyhton3
+
+def fizzbuzz():
+
+    for l in range(1, 101):
+        if l % 3 == 0 and l % 5 != 0:
+            print(" Fizz", end="")
+        elif l % 5 == 0 and l % 3 != 0:
+            print(" Buzz", end="")
+        elif l % 3 == 0 and l % 5 == 0:
+            print(" FizzBuzz", end="")
+        elif l == 1:
+            print (l, end="")
+        else:
+            print(f" {l}", end="")
