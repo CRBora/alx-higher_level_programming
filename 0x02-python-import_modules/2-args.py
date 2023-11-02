533edf81b82482d4a38d6c08ed9b329149808bd2
Llala <rene.raboshakga@gmail.com>
2023-11-02 18:41:55 +0200

    Task 2 printing the number of arguments plus the arguments given

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
         arg_len = len(argv) - 1
 
         if arg_len == 1:
-            print("{} argument".format(arg_len))
+            print("{} argument:".format(arg_len))
         else:
-            print("{} arguments".format(arg_len))
+            print("{} arguments:".format(arg_len))
 
 
         for l, arg in enumerate(argv[1: ], start=1):
