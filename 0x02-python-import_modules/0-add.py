6f28292519c208f8524b765df4a85951a5270c3d
Llala <rene.raboshakga@gmail.com>
2023-11-02 17:36:04 +0200

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-a = 1
-b = 2
+if __name__ == "__0-add__":
 
-from add_0 import add
-result = add(a, b)
+    a = 1
+    b = 2
 
-print("{} + {} = {}".format(a, b, result))
+    from add_0 import add
+    result = add(a, b)
+
+    print("{} + {} = {}".format(a, b, result))
