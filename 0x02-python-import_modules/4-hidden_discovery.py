e8604e9755f67894334a30a36a42dec7a47d0029
Llala <rene.raboshakga@gmail.com>
2023-11-02 20:18:11 +0200

    Task 4 code corrections

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-
+    import os
     import importlib.util
-    spec = importlib.util.spec_from_file_location("hidden_4",\
-    "/root/ALX/alx-higher_level_programming/0x02-python-import_modules/hidden_4.pyc")
+
+    cur_dir = os.getcwd()
+
+    mod_name = "hidden_4.pyc"
+
+    mod_path = os.path.join(cur_dir, mod_name)
+
+    spec = importlib.util.spec_from_file_location("hidden_4", mod_path)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
 
     names = [name for name in dir(module) if not name.startswith("__")]
+
     names.sort()
 
     for name in names:
         print(name)
-
