3bd993f20afa6808a9ef264fdb9a206f449d0442
Llala <rene.raboshakga@gmail.com>
2023-12-16 09:38:16 +0200

    Task 4 chacking for different elements in 2 lists

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    elements_set = set()
+    for element in set_1:
+        if element not in set_2:
+            elements_set.add(element)
+    for element in set_2:
+        if element not in set_1:
+            elements_set.add(element)
+    return elements_set
+
